33ee7d1867d2ba5e5e967201b2aca721a989705f
haojie06 <haojie06@qq.com>
2019-07-28 19:30:58 +0800

    delete old

--- LogSystem/src/system.ts
+++
@@ -1,7 +0,0 @@
-export type MySystem = SystemType<PrivSystem>;
-
-interface PrivSystem extends IStoneServerSystem<PrivSystem> {}
-
-checkApiLevel(1);
-
-export const system = server.registerSystem<MySystem>(0, 0);
