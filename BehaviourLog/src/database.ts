3d1a9ddd64905fd50031f4e4b355c21448d341fc
haojie06 <haojie06@qq.com>
2019-07-28 18:00:34 +0800

    支持1.12的日志系统

---
+++ BehaviourLog/src/database.ts
@@ -0,0 +1,168 @@
+//关于数据库的文件
+function fix(arr: TemplateStringsArray) {
+    return arr
+      .join("")
+      .replace(/\(\n\s+/g, "(")
+      .replace(/\n\s+\)/g, ")")
+      .replace(/\s+/g, " ");
+  }
+  //创建一个储存行为日志的表
+  //记录格式 id,时间,玩家名,玩家坐标，行为，行为对象，对象坐标
+  export const CREATE_TABLE = fix`
+  CREATE TABLE IF NOT EXISTS log(
+    id,
+    time TEXT NOT NULL,
+    name TEXT NOT NULL,
+    playerX INT NOT NULL,
+    playerY INT NOT NULL,
+    playerZ INT NOT NULL,
+    action TEXT NOT NULL,
+    target TEXT NOT NULL,
+    targetX INT NOT NULL,
+    targetY INT NOT NULL,
+    targetZ INT NOT NULL,
+    dim TEXT NOT NULL,
+    desc TEXT,
+    timestamp INT NOT NULL
+  );`;
+
+  //添加记录
+  export const INSERT_LOG = fix`
+  INSERT INTO log (
+    time, name, playerX, playerY, playerZ, action, target, targetX, targetY, targetZ, dim, desc, timestamp
+  ) values (
+    $time, $name, $pX, $pY, $pZ, $action, $target, $tX, $tY, $tZ, $dim, $desc, $timestamp
+  );`;
+//获得一个玩家的所有行为
+  export const SELECT_LOG_BY_NAME = fix`SELECT * FROM log WHERE name=$name;`;
+//获得所有的日志
+  export const SELECT_ALL_LOG = fix`SELECT * FROM log;`;
+
+  //export const DELETE_WARP_BY_NAME = fix`DELETE FROM warp WHERE name=$name;`;
+  export const DELETE_LOG_BY_DAY = fix`DELETE FROM log WHERE timestamp<$timestamp;`;
+
+//获得一个范围内（x y z）的所有记录
+ export const SELECT_ALL_IN_ZONE = fix`SELECT * FROM log WHERE targetX >= $minX AND targetY >= $minY AND targetZ >= $minZ AND targetX <= $maxX AND targetY <= $maxY AND targetZ <= $maxZ AND dim = $dim`;
+ export const SELECT_IN_ZONE_BYACTION = fix`SELECT * FROM log WHERE targetX >= $minX AND targetY >= $minY AND targetZ >= $minZ AND targetX <= $maxX AND targetY <= $maxY AND targetZ <= $maxZ AND dim = $dim AND action = $action`;
+//新增一个约束条件 几小时之内
+ export const SELECT_ALL_IN_ZONE_AFTERTIME = fix`SELECT * FROM log WHERE targetX >= $minX AND targetY >= $minY AND targetZ >= $minZ AND targetX <= $maxX AND targetY <= $maxY AND targetZ <= $maxZ AND dim = $dim AND timestamp > $timeline`;
+ export const SELECT_IN_ZONE_BYACTION_AFTERTIME = fix`SELECT * FROM log WHERE targetX >= $minX AND targetY >= $minY AND targetZ >= $minZ AND targetX <= $maxX AND targetY <= $maxY AND targetZ <= $maxZ AND dim = $dim AND action = $action AND timestamp > $timeline`;
+
+ export const SELECT_ALL_IN_ZONE_AFTERTIME_PNAME = fix`SELECT * FROM log WHERE targetX >= $minX AND targetY >= $minY AND targetZ >= $minZ AND targetX <= $maxX AND targetY <= $maxY AND targetZ <= $maxZ AND dim = $dim AND timestamp > $timeline AND name=$player`;
+ export const SELECT_IN_ZONE_BYACTION_AFTERTIME_PNAME = fix`SELECT * FROM log WHERE targetX >= $minX AND targetY >= $minY AND targetZ >= $minZ AND targetX <= $maxX AND targetY <= $maxY AND targetZ <= $maxZ AND dim = $dim AND action = $action AND timestamp > $timeline AND name=$player`;
+
+export var db = new SQLite3("behaviourLogs.db");
+db.exec(CREATE_TABLE);
+
+//向数据库中添加记录
+export function addRecord($time, $name, $pX, $pY, $pZ, $action, $target, $tX, $tY, $tZ, $dim, $desc=""):void{
+    let $timestamp = new Date().getTime();
+    db.update(INSERT_LOG,{$time, $name, $pX, $pY, $pZ, $action, $target, $tX, $tY, $tZ, $dim, $desc, $timestamp});
+}
+
+//删除几天以前的日志
+export function delRecord(day:number):number{
+  let $timestamp = new Date().getTime() - day * 24 * 60 * 60 * 1000;
+  let delNum =  db.update(DELETE_LOG_BY_DAY,{$timestamp});
+  return delNum;
+}
+export function closeDB(){
+db = null;
+}
+export function readRecord($sX, $sY, $sZ, $eX, $eY, $eZ, $dim, $action="all", $hour=0,$player=""){
+    let $minX = Math.min($sX,$eX);
+    let $minY = Math.min($sY,$eY);
+    let $minZ = Math.min($sZ,$eZ);
+    let $maxX = Math.max($sX,$eX);
+    let $maxY = Math.max($sY,$eY);
+    let $maxZ = Math.max($sZ,$eZ);
+
+    let result = new Array();
+    let datas;
+    let selects = ["break","place","open","use"];
+    if(selects.indexOf($action) == -1){
+        //无效action使用默认all
+        $action = "all";
+    }
+    var length = 0;
+    if ($hour == 0){
+    if($action == "all"){
+        //读出范围内所有的记录 !!!注意 外面为minX 里面的变量也要同名
+          let logs = db.query(SELECT_ALL_IN_ZONE,{$minX, $minY, $minZ, $maxX, $maxY, $maxZ, $dim});
+          datas = Array.from(logs);
+    }
+    else{
+        //读出范围内特定行为的所有记录
+        let logs = db.query(SELECT_IN_ZONE_BYACTION,{$minX, $minY, $minZ, $maxX, $maxY, $maxZ, $dim, $action});
+        datas = Array.from(logs);
+    }
+  }
+  else{
+    //读取几个小时前
+    if ($hour <= 0) throw "hour 需要为正数";
+    let now:number = new Date().getTime();
+    var $timeline = now - $hour * 60 * 60 * 1000;
+    if($action == "all"){
+      var logs;
+      //读出范围内所有的记录 !!!注意 外面为minX 里面的变量也要同名
+      if($player==""){
+      logs = db.query(SELECT_ALL_IN_ZONE_AFTERTIME,{$minX, $minY, $minZ, $maxX, $maxY, $maxZ, $dim, $timeline});
+      }
+      else{
+      logs = db.query(SELECT_ALL_IN_ZONE_AFTERTIME_PNAME,{$minX, $minY, $minZ, $maxX, $maxY, $maxZ, $dim, $timeline, $player});
+      }
+      datas = Array.from(logs);
+  }
+  else{
+      //读出范围内特定行为的所有记录
+      var logs;
+      if ($player == "") {
+      logs = db.query(SELECT_IN_ZONE_BYACTION_AFTERTIME,{$minX, $minY, $minZ, $maxX, $maxY, $maxZ, $dim, $action, $timeline});
+      }
+      else{
+      logs = db.query(SELECT_IN_ZONE_BYACTION_AFTERTIME_PNAME,{$minX, $minY, $minZ, $maxX, $maxY, $maxZ, $dim, $action, $timeline, $player});
+      }
+      datas = Array.from(logs);
+    }
+  }
+    for(let data of datas){
+        var line;
+        switch (data.action) {
+            case "break":
+                line = `<${length + 1}>:${data.time} ${data.name}(${data.playerX},${data.playerY},${data.playerZ}) §c破坏了§r ${data.target}(${data.targetX},${data.targetY},${data.targetZ}) 维度:${dimTran(data.dim)}`;
+                break;
+            case "place":
+                line = `<${length + 1}>:${data.time} ${data.name}(${data.playerX},${data.playerY},${data.playerZ}) §a放置了§r ${data.target}(${data.targetX},${data.targetY},${data.targetZ}) 维度:${dimTran(data.dim)}`;
+                break;
+            case "open":
+                line = `<${length + 1}>:${data.time} ${data.name}(${data.playerX},${data.playerY},${data.playerZ}) §9打开了§r ${data.target}(${data.targetX},${data.targetY},${data.targetZ}) 维度:${dimTran(data.dim)}`;
+                break;
+            case "use":
+                line = `<${length + 1}>:${data.time} ${data.name}(${data.playerX},${data.playerY},${data.playerZ}) §e使用了§r ${data.target}(${data.targetX},${data.targetY},${data.targetZ}) 维度:${dimTran(data.dim)}`;
+                break;
+            default:
+                break;
+        }
+        length = result.push(line);
+    }
+
+    return result;
+  }
+//转换维度名字
+  function dimTran(dim):string{
+    let result:string = "§r未知";
+    switch (Number(dim)) {
+      case 0:
+        result = "§2主世界§r";
+        break;
+      case 1:
+        result = "§4下界§r";
+        break;
+      case 2:
+        result = "§5末地§r";
+      break;
+    default:
+      break;
+    }
+    return result;
+  }
---
+++ BehaviourLog/src/main.ts
@@ -0,0 +1,251 @@
+/* 
+create by haojie06 2019/6/10
+用于记录玩家在游戏中的行为 （超简化版coi..）
+更新 支持1.12
+*/
+import { db,addRecord,readRecord, delRecord, closeDB } from "./database";
+import { getTime,stringToInt,transNum,checkIfContainer} from "./utils";
+
+const system = server.registerSystem(0, 0);
+system.initialize = function() {
+  server.log("日志系统v2.0 loaded https://github.com/haojie06/BedrockPlugins");
+  //方块破坏记录
+  system.listenForEvent("minecraft:player_destroyed_block", (data) => {
+    let player = data.data.player;
+    let playerName = getName(player);
+    let dim = getDimensionOfEntity(player);
+    let playerPos = getPosCmp(player);
+    let blockName = data.data.block_identifier.split(":")[1];
+    let bPostion = data.data.block_position;
+
+    let pX = transNum(playerPos.x);
+    let pY = transNum(playerPos.y);
+    let pZ = transNum(playerPos.z);
+
+    let bX = transNum(bPostion.x);
+    let bY = transNum(bPostion.y);
+    let bZ = transNum(bPostion.z);
+
+    let time = getTime();
+    //system.sendText(player,`${time} ${playerName}(${pX},${pY},${pZ}) 破坏 ${blockName}(${bX},${bY},${bZ}) dim:${dim}`);
+    addRecord(time, playerName, pX, pY, pZ, "break", blockName, bX, bY, bZ, dim);
+  });
+
+  //使用物品/
+  system.listenForEvent("minecraft:entity_use_item",(data)=>{
+    let entity = data.data.entity;
+    if (entity.__identifier__ == "minecraft:player") {
+      let method = data.data.use_member;
+      let playerName = getName(entity);
+      let itemStacks = data.data.item_stack;
+      let dim = getDimensionOfEntity(entity);
+      let playerPos = getPosCmp(entity);
+
+      let pX = transNum(playerPos.x);
+      let pY = transNum(playerPos.y);
+      let pZ = transNum(playerPos.z);
+      let time = getTime();
+      let itemName = itemStacks.item.split(":")[1];
+      //放置方块的记录由下一个方法实现 这里目前主要记录倒演讲
+      if(method != "place"){
+        //system.sendText(entity,`${time} ${playerName}(${pX},${pY},${pZ}) ${method} ${itemStacks.item} dim:${dim}`);
+        addRecord(time, playerName, pX, pY, pZ, "use", itemName, pX, pY, pZ, dim);        
+      }
+    }
+  });
+//容器交互
+  system.listenForEvent("minecraft:block_interacted_with",(data)=>{
+    let player = data.data.player;
+    let bPostion = data.data.block_position;
+    let playerName = getName(player);
+    let dim = getDimensionOfEntity(player);
+    let playerPos = getPosCmp(player);
+    let bX = transNum(bPostion.x);
+    let bY = transNum(bPostion.y);
+    let bZ = transNum(bPostion.z);
+
+    let pX = transNum(playerPos.x);
+    let pY = transNum(playerPos.y);
+    let pZ = transNum(playerPos.z);
+
+    let time = getTime();
+    //获得方块
+    let tickAreaCmp = system.getComponent<ITickWorldComponent>(player,MinecraftComponent.TickWorld);
+    let tickingArea = tickAreaCmp.data.ticking_area;
+    let block = system.getBlock(tickingArea,bX,bY,bZ);
+    let blockName = block.__identifier__;
+    if(checkIfContainer(blockName)){
+    blockName = blockName.split(":")[1];
+    system.sendText(player,`${time} ${playerName}(${pX},${pY},${pZ}) 容器交互 ${blockName}(${bX},${bY},${bZ}) dim:${dim}`);
+    addRecord(time, playerName, pX, pY, pZ, "open", blockName, bX, bY, bZ, dim);  
+    }
+  });
+
+  //放置方块记录
+  system.listenForEvent("minecraft:player_placed_block",(data)=>{
+    let player = data.data.player;
+    let bPostion = data.data.block_position;
+    let playerName = getName(player);
+    let dim = getDimensionOfEntity(player);
+    let playerPos = getPosCmp(player);
+    let bX = transNum(bPostion.x);
+    let bY = transNum(bPostion.y);
+    let bZ = transNum(bPostion.z);
+
+    let pX = transNum(playerPos.x);
+    let pY = transNum(playerPos.y);
+    let pZ = transNum(playerPos.z);
+
+    let time = getTime();
+    //获得方块
+    let tickAreaCmp = system.getComponent<ITickWorldComponent>(player,MinecraftComponent.TickWorld);
+    let tickingArea = tickAreaCmp.data.ticking_area;
+    let block = system.getBlock(tickingArea,bX,bY,bZ);
+    let blockName = block.__identifier__.split(":")[1];
+    //system.sendText(player,`${time} ${playerName}(${pX},${pY},${pZ}) 放置 ${blockName}(${bX},${bY},${bZ}) dim:${dim}`);
+    addRecord(time, playerName, pX, pY, pZ, "place", blockName, bX, bY, bZ, dim);  
+  });
+
+  //添加查询命令
+// /logs x y z x y z 可选：行为
+// /logsof playerName 暂时不可用softenum
+//system.registerSoftEnum("action_enum", ["all","break","place","open","use"]);
+
+system.registerCommand("logs", {
+  description: "读取行为日志",
+  permission: 1,
+  overloads: [
+    {
+      parameters: [{
+        name:"start",
+        type:"position"
+      },
+      {
+        name:"end",
+        type:"position"
+      },
+      {//可选的行为名称 （破坏 放置 打开 使用）
+        name:"行为名",
+        type:"string",
+        optional:true
+      },
+      {
+        name:"几小时内",
+        type:"int",
+        optional:true
+      },
+      {
+        name:"玩家名",
+        type:"string",
+        optional:true
+      }
+    ],
+      handler([start,end,action,hour,player]) {
+        if (!this.entity) throw "Player required";
+        let entity = this.entity;
+        let sX = transNum(start.x);
+        let sY = transNum(start.y);
+        let sZ = transNum(start.z);
+
+        let eX = transNum(end.x);
+        let eY = transNum(end.y);
+        let eZ = transNum(end.z);
+
+        
+        let dim = getDimensionOfEntity(entity);
+        let records:string[];
+        if (hour < 0 ){
+          hour = 0;
+        }
+        if(action == ""){
+          //server.log(`全局查找 ${sX} ${sY} ${sZ}`);
+          records = readRecord(sX,sY,sZ,eX,eY,eZ,dim,"all",hour,player);
+        }
+        else{
+          //server.log("特定行为查找" + action);
+          records = readRecord(sX,sY,sZ,eX,eY,eZ,dim,action,hour,player);
+        }
+        let say:string = `§a§l日志系统2.0 by haojie06 以下为查找到的记录：§r\n`;
+        if (hour == 0){
+          say += `§b所有时间 `;
+        }
+        else{
+          say += `§b${hour}小时内 `;
+        }
+
+        if (player == ""){
+          say += `§6所有玩家的`;
+        }
+        else{
+          say += `§6玩家${player}的§r`;
+        }
+
+        if (action == "break"){
+          say += `§c破坏行为记录§r:\n`;
+        }
+        else if (action == "place"){
+          say += `§9放置行为记录§r:\n`;
+        }
+        else if (action == "open"){
+          say += `§e开箱行为记录§r:\n`;
+        }
+        else if (action == "use"){
+          say += `§3使用行为记录§r:\n`;
+        }
+        else{
+          say += `§a所有行为的记录§r:\n`;
+        }
+
+        for(let line of records){
+          say = say + line + "\n";
+        }
+        
+        system.sendText(entity,`${say}`);
+      }
+    } as CommandOverload<["position","position","string","int","string"]>
+  ]
+});
+
+system.registerCommand("dellogs",{
+  description:"删除几天以前的所有日志",
+  permission:1,
+  overloads:[{
+    parameters:[{
+      name:"几天以前",
+      type:"int"
+    }],
+    handler([day]){
+      if(!this.entity) throw "只有玩家可以执行该命令";
+      let delNum = delRecord(day);
+      system.sendText(this.entity,`§a已删除${day}天前共计:${delNum}条记录`);
+    }
+  } as CommandOverload<["int"]>
+]
+});
+};
+
+system.shutdown = function() {
+  //在此处进行结束工作
+  closeDB();
+  server.log("日志系统已卸载");
+};
+
+function getName(entity: IEntity) {
+  return system.getComponent<INameableComponent>(entity, MinecraftComponent.Nameable).data.name;
+}
+
+function getPosCmp(entity: IEntity){
+  return system.getComponent<IPositionComponent>(entity,MinecraftComponent.Position).data;
+}
+
+function getDimensionOfEntity(entity: IEntity){
+  let dimension;
+  if (system.hasComponent(entity, "stone:dimension")) {
+    let comp = system.getComponent(entity,MinecraftComponent.Dimension);
+    dimension = comp.data;
+}
+  else{
+    dimension = "无法获得维度";
+  }
+  return String(dimension);
+}
