3d38d273397429b8d11a15869aa1674c76aae76e
haojie06 <haojie06@qq.com>
2019-07-29 19:08:21 +0800

    交换上下

--- Elevator/src/main.ts
+++ Elevator/src/main.ts
@@ -2,7 +2,7 @@ const system = server.registerSystem(0, 0);
 //电梯方块是什么 这里用的是铁块
 let eBlockName = "minecraft:iron_block";
 //方块之间最大上升距离 这里是15格
-let maxUpDis = 15;
+let maxUpDis = 20;
 system.initialize = function() {
     server.log("Elevator Plugin Loaded");
     system.listenForEvent("minecraft:block_interacted_with",(data)=>{
@@ -31,15 +31,15 @@ system.initialize = function() {
                     //system.sendText(player,`已找到向上电梯方块`);
                     system.executeCommand(`playsound tile.piston.in @a[name="${getName(player)}"] ${bX} ${bY} ${bZ} 1 1`,data=>{});
                     let pcmp = system.getComponent<IPositionComponent>(player,MinecraftComponent.Position);
-                    pcmp.data.y = bY + i + 1;
+                    pcmp.data.y = bY - i + 1;
                     system.applyComponentChanges(player,pcmp);
-                    system.executeCommand(`playsound tile.piston.out @a[name="${getName(player)}"] ${bX} ${bY+ i +1} ${bZ} 1 1`,data=>{});
+                    system.executeCommand(`playsound tile.piston.out @a[name="${getName(player)}"] ${bX} ${bY - i +1} ${bZ} 1 1`,data=>{});
                     ifFind = true;
                     i = maxUpDis + 1;
                 }
             }
             if(!ifFind){
-                system.sendText(player, "未在上方找到电梯方块");
+                system.sendText(player, "未在下方找到电梯方块");
             }
         }
     });
@@ -70,15 +70,15 @@ system.initialize = function() {
                     //system.sendText(player,`已找到向下电梯方块`);
                     system.executeCommand(`playsound tile.piston.in @a[name="${getName(player)}"] ${bX} ${bY} ${bZ} 1 1`,data=>{});
                     let pcmp = system.getComponent<IPositionComponent>(player,MinecraftComponent.Position);
-                    pcmp.data.y = bY - i + 1;
+                    pcmp.data.y = bY + i + 1;
                     system.applyComponentChanges(player,pcmp);
-                    system.executeCommand(`playsound tile.piston.out @a[name="${getName(player)}"] ${bX} ${bY - i +1} ${bZ} 1 1`,data=>{});
+                    system.executeCommand(`playsound tile.piston.out @a[name="${getName(player)}"] ${bX} ${bY + i +1} ${bZ} 1 1`,data=>{});
                     i = maxUpDis + 1;
                     ifFind = true;
                 }
             }
             if(!ifFind){
-                system.sendText(player, "未在下方找到电梯方块");
+                system.sendText(player, "未在上方找到电梯方块");
             }
         }
     });
